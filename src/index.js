52628b612420274d9cf94257bd29074f9277e51e
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-01-26 06:48:27 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit",toDO)
 });
+const toDo = event => {
+  event.preventDefault();
+  const task = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = task.value;
+
+  document.getElementById("tasks").appendChild(newTask);
+}
